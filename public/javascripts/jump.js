dddb2c706a1e466cfdc9e92760281cea86bc8d34
ksky521 <ksky521@gmail.com>
2018-01-03 23:04:41 +0800

    修改

--- public/javascripts/jump.js
+++ public/javascripts/jump.js
@@ -1,7 +1,3 @@
-const playerR = 40;
-const playerG = 43;
-const playerB = 86;
-
 function getImageData(src) {
     const img = new Image();
     return new Promise((resolve, reject) => {
@@ -15,116 +11,165 @@ function getImageData(src) {
             let imgData = ctx.getImageData(0, 0, width, height);
             resolve(imgData);
         };
-        img.onerror = (e) => {
+        img.onerror = e => {
             img.onerror = img.onload = null;
             reject(e);
         };
         img.src = src;
     });
 }
-let count = 0;
 function tolerenceHelper(r, g, b, rt, gt, bt, t) {
-    return r > rt - t && r < rt + t && g > gt - t && g < gt + t && b > bt - t && b < bt + t;
+    return (
+        r > rt - t &&
+        r < rt + t &&
+        g > gt - t &&
+        g < gt + t &&
+        b > bt - t &&
+        b < bt + t
+    );
 }
 
-function getPlayerPos(img) {
-    return new Promise((resolve, reject) => {
-        getImageData(img).then(({data, width, height}) => {
-            let minX = Infinity;
-            let maxX = -1;
-            let maxY = -1;
-            let minY = Infinity;
-            let pos = [0, 0];
+function getCurCenter(data, width, height) {
+    // 小人的颜色值
+    const playerR = 40;
+    const playerG = 43;
+    const playerB = 86;
 
-            let startY = Math.floor(height / 4);
-            let endY = Math.floor(height * 3 / 4);
+    let minX = Infinity;
+    let maxX = -1;
+    let maxY = -1;
+    // 找到小人当前的底部位置
+    let pos = [0, 0];
 
-            for (let x = 0; x < width; x++) {
-                for (let y = startY; y < endY; y++) {
-                    let pixel = y * (width * 4) + x * 4;
-                    let r = data[pixel];
-                    let g = data[pixel + 1];
-                    let b = data[pixel + 2];
-                    if (y > pos[1] && tolerenceHelper(r, g, b, playerR, playerG, playerB, 16)) {
-                        minX = Math.min(minX, x);
-                        minY = Math.min(minY, y);
-                        maxX = Math.max(maxX, x);
-                        maxY = Math.max(maxY, y);
-                    }
-                }
+    let startY = Math.floor(height / 4);
+    let endY = Math.floor(height * 3 / 4);
+    for (let x = 0; x < width; x++) {
+        for (let y = startY; y < endY; y++) {
+            let i = y * (width * 4) + x * 4;
+            let r = data[i];
+            let g = data[i + 1];
+            let b = data[i + 2];
+            if (
+                y > pos[1] &&
+                tolerenceHelper(r, g, b, playerR, playerG, playerB, 16)
+            ) {
+                minX = Math.min(minX, x);
+                maxX = Math.max(maxX, x);
+                maxY = Math.max(maxY, y);
             }
-            pos[0] = (maxX + minX) / 2;
-            pos[1] = maxY;
-            console.log(`player position (x, y)= (${pos[0]}, ${pos[1]})`);
+        }
+    }
+    pos[0] = Math.floor((maxX + minX) / 2);
+    pos[1] = maxY;
+    console.log(`player position (x, y)= (${pos[0]}, ${pos[1]})`);
+    return pos;
+}
+function getNextCenter(data, width, height, y = -1) {
+    let startY = Math.floor(height / 4);
+    let endY = Math.floor(height * 3 / 4);
 
-            // 在当前位置的上面
-
-            let r = data[0],
-                g = data[1],
-                b = data[2];
-
-            let pixel = [];
-            let nextPos = [0, 0];
-            let nextMaxY = -1,
-                nextMinY = Infinity;
-            // 保证从当前小人位置顶点往上
-            endY = Math.min(endY, minY + 30);
-            let endX = width;
-            for (let y = startY; y < endY; y++) {
-                for (let x = 0; x < endX; x++) {
-                    let i = y * (width * 4) + x * 4;
-                    let rt = data[i];
-                    let gt = data[i + 1];
-                    let bt = data[i + 2];
-                    // 不是默认背景颜色
-                    if (!tolerenceHelper(rt, gt, bt, r, g, b, 30)) {
-                        if (pixel.length === 0) {
-                            //找出顶点
-                            pixel = [rt, gt, bt];
-                            nextPos[0] = x;
-                            // 修正范围,只可能是顶点为上中心y+200的
-                            endY = y + 200;
-                            endX = x + 20;
-                            break;
-                        } else {
-                            if (x > nextPos[0] - 200 && tolerenceHelper(rt, gt, bt, pixel[0], pixel[1], pixel[2], 5)) {
-                                // console.log(x, y);
-                                //存在顶点了，则根据颜色值开始匹配
-
-                                nextMaxY = Math.max(nextMaxY, y);
-                                nextMinY = Math.min(nextMinY, y);
+    // 去除背景色
+    let startX = startY * width * 4;
+    let r = data[startX],
+        g = data[startX + 1],
+        b = data[startX + 2];
+    let maxY = -1;
+    let apex = [];
+    let pos = [0, 0];
+    // 保证从当前小人位置底部点往上
+    endY = Math.min(endY, y);
+    let endX = width;
+    let gapCount = 0;
+    for (let y = startY; y < endY; y++) {
+        let find = 0;
+        for (let x = 1; x < endX; x++) {
+            let i = y * (width * 4) + x * 4;
+            let rt = data[i];
+            let gt = data[i + 1];
+            let bt = data[i + 2];
+            // 不是默认背景颜色
+            if (!tolerenceHelper(rt, gt, bt, r, g, b, 30)) {
+                if (apex.length === 0) {
+                    if (
+                        !tolerenceHelper(
+                            data[i + 4],
+                            data[i + 5],
+                            data[i + 6],
+                            r,
+                            g,
+                            b,
+                            30
+                        )
+                    ) {
+                        //椭圆形找中心，往后找30个像素点
+                        let len = 2;
+                        while (len++ !== 30) {
+                            i += len * 4;
+                            if (
+                                tolerenceHelper(
+                                    data[i + 4],
+                                    data[i + 5],
+                                    data[i + 6],
+                                    r,
+                                    g,
+                                    b,
+                                    30
+                                )
+                            ) {
+                                break;
                             }
                         }
+                        x += len;
                     }
+                    //找出顶点
+                    apex = [rt, gt, bt, x, y];
+                    pos[0] = x;
+                    // 减少循环范围
+                    endX = x;
+                    break;
+                } else if (
+                    tolerenceHelper(rt, gt, bt, apex[0], apex[1], apex[2], 5)
+                ) {
+                    //存在顶点了，则根据颜色值开始匹配
+                    maxY = Math.max(maxY, y);
+                    find = x;
+                    break;
                 }
             }
-            nextPos[1] = (nextMaxY + nextMinY) / 2;
-            // console.log(points_top, points_left, points_right);
-            console.log(`next position center (x,y)=${nextPos[0]},${nextPos[1]}`);
-
-            resolve({
-                player: pos,
-                nextPos
-            });
-        });
-    });
+        }
+        if (apex.length !== 0 && !find) {
+            gapCount++;
+        }
+        if (gapCount === 3) {
+            break;
+        }
+    }
+    pos[1] = Math.floor((maxY + apex[4]) / 2);
+    // console.log(points_top, points_left, points_right);
+    console.log(`next position center (x,y)=${pos[0]},${pos[1]}`);
+    return pos;
+}
+async function getPosition(img) {
+    let { data, width, height } = await getImageData(img);
+    let pos1 = getCurCenter(data, width, height);
+    let pos2 = getNextCenter(data, width, height, pos1[1]);
+    return { pos1, pos2 };
 }
 
+function getPointHtml(top, left){
+    return `<div class="point" style="top:${top}px;left:${left}px">`
+}
 let html = '';
 for (let i = 1; i <= 12; i++) {
     let url = `/images/demos/${i}.png`;
     html += `<li><img src="${url}" id="img${i}"/></li>`;
-    getPlayerPos(url).then(({player, nextPos}) => {
-        console.log(i);
-        let pos = $(`#img${i}`).position();
-        let div = $(`<div style="background:red;width: 10px;height:10px;
-        position:absolute;z-index:999;top:${pos.top + player[1] - 5}px;left:${pos.left + player[0] - 5}px"></div>`);
-
-        div.appendTo($('body'));
-        let div2 = $(`<div style="background:red;width: 10px;height:10px;
-        position:absolute;z-index:999;top:${pos.top + nextPos[1] - 5}px;left:${pos.left + nextPos[0] - 5}px"></div>`);
-
-        div2.appendTo($('body'));
+    getPosition(url).then(({ pos1, pos2 }) => {
+        let {top, left} = $(`#img${i}`).position();
+        let $curPoint = $(getPointHtml(top + pos1[1] - 5, left+pos1[0] - 5));
+        $curPoint.appendTo($('body'));
+        
+        let $nextPoint = $(getPointHtml(top + pos2[1] - 5, left+pos2[0] - 5));
+        $nextPoint.appendTo($('body'));
     });
 }
 $('#list').html(html);
